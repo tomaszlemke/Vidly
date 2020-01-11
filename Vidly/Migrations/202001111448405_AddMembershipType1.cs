62d81e81e567068ce00a364ef7ff50d71d5a0806
tomaszlemke <tlemke86@gmail.com>
2020-01-11 16:02:35 +0100

    seeding db

---
+++ Vidly/Migrations/202001111448405_AddMembershipType1.cs
@@ -0,0 +1,20 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMembershipType1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "SignUpFee", c => c.Short(nullable: false));
+            DropColumn("dbo.MembershipTypes", "SignUpFree");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.MembershipTypes", "SignUpFree", c => c.Short(nullable: false));
+            DropColumn("dbo.MembershipTypes", "SignUpFee");
+        }
+    }
+}
